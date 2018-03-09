09228df5529f1ef7e500387aa2c8ed31b52e6ab3
Daring the CORD <dariuszbz@wp.pl>
2018-03-09 13:36:02 +0000

    Add project files.

---
+++ WebDocs/Models/WebDoc.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using ImplementationClasses;
+
+namespace WebDocs.Models
+{
+    public class WebDoc : ImplementationClasses.ImplementationActorProp
+    {
+        /*
+         * Followin the API url:
+         * https://localhost:44391/api/Documents - see: architecture project
+         * read the returned properties.
+         * Some opac required for the json response/request and the view
+         */
+
+        [ReadOnly(true)]
+        [UIHint("readonly")]
+        [DisplayName("Document ID")]
+        [JsonProperty("ActorPropID")]
+        public Guid WebDocID { get => base.ActorPropID; set => base.ActorPropID = value; }
+
+        [ReadOnly(true)]
+        [UIHint("readonly")]
+        [DisplayName("Document Owner ID")]
+        [JsonProperty("ActorID")]
+        public Guid WebDocOwnerID { get => base.ActorID; set => base.ActorID = value; }
+
+        [DisplayName("Document Title")]
+        [StringLength(250, MinimumLength = 3)]
+        [Required(ErrorMessage = "Can't be empty or less than 3 characters")]
+        [JsonProperty("ActorPropTitle")]
+        public string WebDocTitle { get => base.ActorPropTitle; set => base.ActorPropTitle = value; }
+
+        [ReadOnly(true)]
+        [UIHint("readonly")]
+        [DisplayName("Document Number")]
+        [JsonProperty("Number")]
+        public string WebDocNumber { get; set; }
+    }
+}
---
+++ WebDocs/Proxy/WebDocsProxy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebDocs.Models;
+
+
+namespace WebDocs.Proxy
+{
+    public static class WebDocsProxy
+    {
+        static HttpClient client = new HttpClient();
+        private const string apiPath = "api/Documents/";
+
+        static WebDocsProxy()
+        {
+            client.BaseAddress = new Uri("https://localhost:44391/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public static List<WebDoc> GetDocsFromApi()
+        {
+            List<WebDoc> documents = null;
+            HttpResponseMessage response = client.GetAsync("api/Documents/").Result;
+            
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                documents = JsonConvert.DeserializeObject<List<WebDoc>>(json);
+            }
+            return documents;
+        }
+    }
+}
